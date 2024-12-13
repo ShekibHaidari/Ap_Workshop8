6f46be3f983d609bd7e4942466f05c9c5e810d85
AhmadShekibHaidari <ahmadshekibhaidari82@gmail.com>
2024-12-13 23:46:16 +0330

    Note class degugged

--- untitled/src/Note.java
+++ untitled/src/Note.java
@@ -4,24 +4,69 @@ import java.time.LocalDate;
 public class Note implements Serializable {
     private String title;
     private String content;
-    private LocalDate date;
+    private LocalDate creationDate;
 
     public Note(String title, String content) {
         this.title = title;
         this.content = content;
-        this.date = LocalDate.now();
+        this.creationDate = LocalDate.now();
     }
+
     public String getTitle() {
         return title;
     }
+
     public String getContent() {
         return content;
     }
-    public LocalDate getDate() {
-        return LocalDate.now();
+
+    public LocalDate getCreationDate() {
+        return creationDate;
     }
+
     @Override
     public String toString() {
-        return "NoteBook{" + "title=" + title + ", content=" + content + ", date=" + date + '}';
+        return "Title: " + title + "\nDate: " + creationDate + "\nContent:\n" + content;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//import java.io.Serializable;
+//import java.time.LocalDate;
+//
+//public class Note implements Serializable {
+//    private String title;
+//    private String content;
+//    private LocalDate date;
+//
+//    public Note(String title, String content) {
+//        this.title = title;
+//        this.content = content;
+//        this.date = LocalDate.now();
+//    }
+//    public String getTitle() {
+//        return title;
+//    }
+//    public String getContent() {
+//        return content;
+//    }
+//    public LocalDate getDate() {
+//        return LocalDate.now();
+//    }
+//    @Override
+//    public String toString() {
+//        return "NoteBook{" + "title=" + title + ", content=" + content + ", date=" + date + '}';
+//    }
+//}
